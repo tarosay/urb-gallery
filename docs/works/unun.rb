9c5b85c470f95fcf249c33779f0c123af8f79283
Tarosa <tarosa.y@gmail.com>
2026-09-19 11:49:49 +0900

    Add files via upload

---
+++ docs/works/unun.rb
@@ -0,0 +1,236 @@
+np = NeoPixel.new(64)
+
+loop do
+  np.brightness(30)
+  np.auto = false
+  np.fill(227, 82, 0)   # いちばん多い色
+  np.set(7, 0, 0, 0)
+  np.set(6, 0, 0, 0)
+  np.set(5, 0, 0, 0)
+  np.set(4, 0, 0, 0)
+  np.set(3, 0, 0, 0)
+  np.set(2, 0, 0, 0)
+  np.set(1, 0, 0, 0)
+  np.set(0, 0, 0, 0)
+  np.set(8, 0, 0, 0)
+  np.set(9, 0, 0, 0)
+  np.set(10, 0, 0, 0)
+  np.set(13, 0, 0, 0)
+  np.set(14, 0, 0, 0)
+  np.set(15, 0, 0, 0)
+  np.set(23, 0, 0, 0)
+  np.set(22, 0, 0, 0)
+  np.set(17, 0, 0, 0)
+  np.set(16, 0, 0, 0)
+  np.set(24, 0, 0, 0)
+  np.set(26, 255, 255, 255)
+  np.set(29, 255, 255, 255)
+  np.set(31, 0, 0, 0)
+  np.set(39, 0, 0, 0)
+  np.set(32, 0, 0, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(227, 82, 0)   # いちばん多い色
+  np.set(7, 0, 0, 0)
+  np.set(6, 0, 0, 0)
+  np.set(5, 0, 0, 0)
+  np.set(4, 0, 0, 0)
+  np.set(3, 0, 0, 0)
+  np.set(2, 0, 0, 0)
+  np.set(1, 0, 0, 0)
+  np.set(0, 0, 0, 0)
+  np.set(8, 0, 0, 0)
+  np.set(9, 0, 0, 0)
+  np.set(10, 0, 0, 0)
+  np.set(11, 0, 0, 0)
+  np.set(14, 0, 0, 0)
+  np.set(15, 0, 0, 0)
+  np.set(23, 0, 0, 0)
+  np.set(22, 0, 0, 0)
+  np.set(21, 0, 0, 0)
+  np.set(16, 0, 0, 0)
+  np.set(24, 0, 0, 0)
+  np.set(25, 0, 0, 0)
+  np.set(27, 255, 255, 255)
+  np.set(30, 255, 255, 255)
+  np.set(39, 0, 0, 0)
+  np.set(38, 0, 0, 0)
+  np.set(40, 0, 0, 0)
+  np.set(55, 0, 0, 0)
+  np.set(56, 0, 0, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(227, 82, 0)   # いちばん多い色
+  np.set(7, 0, 0, 0)
+  np.set(6, 0, 0, 0)
+  np.set(5, 0, 0, 0)
+  np.set(4, 0, 0, 0)
+  np.set(3, 0, 0, 0)
+  np.set(2, 0, 0, 0)
+  np.set(1, 0, 0, 0)
+  np.set(0, 0, 0, 0)
+  np.set(8, 0, 0, 0)
+  np.set(9, 0, 0, 0)
+  np.set(10, 0, 0, 0)
+  np.set(11, 0, 0, 0)
+  np.set(12, 0, 0, 0)
+  np.set(15, 0, 0, 0)
+  np.set(23, 0, 0, 0)
+  np.set(22, 0, 0, 0)
+  np.set(21, 0, 0, 0)
+  np.set(20, 0, 0, 0)
+  np.set(24, 0, 0, 0)
+  np.set(25, 0, 0, 0)
+  np.set(26, 0, 0, 0)
+  np.set(28, 255, 255, 255)
+  np.set(31, 255, 255, 255)
+  np.set(39, 0, 0, 0)
+  np.set(38, 0, 0, 0)
+  np.set(37, 0, 0, 0)
+  np.set(40, 0, 0, 0)
+  np.set(41, 0, 0, 0)
+  np.set(55, 0, 0, 0)
+  np.set(54, 0, 0, 0)
+  np.set(56, 0, 0, 0)
+  np.set(57, 0, 0, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(0, 0, 0)   # いちばん多い色
+  np.set(14, 227, 82, 0)
+  np.set(15, 227, 82, 0)
+  np.set(18, 227, 82, 0)
+  np.set(17, 227, 82, 0)
+  np.set(16, 227, 82, 0)
+  np.set(28, 227, 82, 0)
+  np.set(29, 255, 255, 255)
+  np.set(30, 227, 82, 0)
+  np.set(31, 227, 82, 0)
+  np.set(35, 227, 82, 0)
+  np.set(34, 227, 82, 0)
+  np.set(33, 227, 82, 0)
+  np.set(32, 227, 82, 0)
+  np.set(43, 227, 82, 0)
+  np.set(44, 227, 82, 0)
+  np.set(45, 227, 82, 0)
+  np.set(46, 227, 82, 0)
+  np.set(47, 227, 82, 0)
+  np.set(52, 227, 82, 0)
+  np.set(51, 227, 82, 0)
+  np.set(50, 227, 82, 0)
+  np.set(49, 227, 82, 0)
+  np.set(48, 227, 82, 0)
+  np.set(59, 227, 82, 0)
+  np.set(60, 227, 82, 0)
+  np.set(61, 227, 82, 0)
+  np.set(62, 227, 82, 0)
+  np.set(63, 227, 82, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(0, 0, 0)   # いちばん多い色
+  np.set(15, 227, 82, 0)
+  np.set(17, 227, 82, 0)
+  np.set(16, 227, 82, 0)
+  np.set(29, 227, 82, 0)
+  np.set(30, 255, 255, 255)
+  np.set(31, 227, 82, 0)
+  np.set(34, 227, 82, 0)
+  np.set(33, 227, 82, 0)
+  np.set(32, 227, 82, 0)
+  np.set(44, 227, 82, 0)
+  np.set(45, 227, 82, 0)
+  np.set(46, 227, 82, 0)
+  np.set(47, 227, 82, 0)
+  np.set(51, 227, 82, 0)
+  np.set(50, 227, 82, 0)
+  np.set(49, 227, 82, 0)
+  np.set(48, 227, 82, 0)
+  np.set(60, 227, 82, 0)
+  np.set(61, 227, 82, 0)
+  np.set(62, 227, 82, 0)
+  np.set(63, 227, 82, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(0, 0, 0)   # いちばん多い色
+  np.set(16, 227, 82, 0)
+  np.set(30, 227, 82, 0)
+  np.set(31, 255, 255, 255)
+  np.set(33, 227, 82, 0)
+  np.set(32, 227, 82, 0)
+  np.set(45, 227, 82, 0)
+  np.set(46, 227, 82, 0)
+  np.set(47, 227, 82, 0)
+  np.set(50, 227, 82, 0)
+  np.set(49, 227, 82, 0)
+  np.set(48, 227, 82, 0)
+  np.set(61, 227, 82, 0)
+  np.set(62, 227, 82, 0)
+  np.set(63, 227, 82, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(0, 0, 0)   # いちばん多い色
+  np.set(31, 227, 82, 0)
+  np.set(32, 227, 82, 0)
+  np.set(46, 227, 82, 0)
+  np.set(47, 227, 82, 0)
+  np.set(49, 227, 82, 0)
+  np.set(48, 227, 82, 0)
+  np.set(62, 227, 82, 0)
+  np.set(63, 227, 82, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(0, 0, 0)   # いちばん多い色
+  np.set(47, 227, 82, 0)
+  np.set(48, 227, 82, 0)
+  np.set(63, 227, 82, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(0, 0, 0)   # いちばん多い色
+  np.set(47, 227, 82, 0)
+  np.set(48, 227, 82, 0)
+  np.set(63, 227, 82, 0)
+  np.show
+  np.auto = true
+  wait_ms 500
+  np.auto = false
+  np.fill(0, 0, 0)   # いちばん多い色
+  np.show
+  np.auto = true
+  wait_ms 500
+end
+
+# ------------------------------------------------------------
+# ここから下は URB Block Lab のブロックです。消すとブロックに戻せません。
+# 上の Ruby を書きかえても、読みこむときはこちらが使われます。
+# ------------------------------------------------------------
+# urb-block/1 7dhbb6pKFADg39JBk2pRAQEVY9BStbYqtYrXKEUuikVQQMQL/vYTrbu13b1t44vJmQcWazAra+YDYliBvmaIzxagVkAT9MFM
+# urb-block/1 GMh12bRUQwcUAr9e7ayAvZjIgAIzs88bOm/ZgmkDGKgSoACdV+mLNFoto67x6FiPYmQkARi4gMLjKAwWgAqRhAe/q6EYpuzI
+# urb-block/1 5p8STVbDA13YvXaFzbIYfbiURX1fAtlVQBMIDFR9MrN3zd6w2+Ouve3JQWFdNvi+qQ6Gti5b1p/6RYyNI0s05PUqQa+eHD1J
+# urb-block/1 l00AA0WVNWlXsJ4pAiqKeDDQZdf+rvhEUPXXpZvTyezivt5NxLpSuMktjAr69K4uky0Wq4AC6NsgiH3YnRAIQSDES0oQ+/D3
+# urb-block/1 ADBgEEABBEZgFEG2KbpPdwm2S1Dk9WoUUCB+OIFvuyAOZwhAAXI3EU9sc3Kbk4e/iG2LvJX9YXfmgmrz49c97xr8IgX5J0wq
+# urb-block/1 5Q5aN4X1EILAIWKpuj1aQ0Ey5gelxoI95PXZuP92e4RFk++54jJS65EOmrmvVMTlu20ucyVAEQjied6/Et5cCv7Nw8jy51Jm
+# urb-block/1 Gk2xi1tH/InwV4Yf4xkSIqtssZyrRSxryva8TX5Tvx4eSSjP8rXHGTQsQs1Zn5s0G08t+1SEiY7LU5wIXZWrNs51+pVCdPoj
+# urb-block/1 4WeG+/SN8kM8Q0ImRyQr4344rCfrSnLI16skfiSh5E9tQgEuE3CgzTKTuL+K5pKnIiSTvsR0ocCLJZ1DiGv7ysDLPxMeGKJ7
+# urb-block/1 w0PSz+IZEqZy7JN0g7o38/5aGFnsYpAsHkkYonuXBU2ptZXggOJYwkHryqkIlcZTLJxDImxcilQh33MAk9K/IESJ95TIe9JP
+# urb-block/1 4hkSXjurZtGXcbB0vaAN8q3CaJU+kjBGx3UoE6pBzUbDly15vXWsfSrC6dqtT7kM4kqGIJf1vug9Ub8h/EiJfPZ0HsYzJCxX
+# urb-block/1 hwiHKRSFM+HgqqWTrZJxJGEl7rMZsaZO3YcgceEYm+799FSEYkZSjXWeaTHRyZV2OZd64fG/EH6g/PB0HsQzJBTuJBdt4qZO
+# urb-block/1 Mbc5L9xv3cvckYSTdiEpoJw/WB6VknjRKaRvL0/2jzTbRqixNR2z2RV0R6h0OkwcQ/gDKXqOhGs2ZifvrhpOJ35HjxtwaRng
+# urb-block/1 jyRsPRhqlMosKgNu0IGDsWDOO9mLlC/pIgTNVphJt9pTDZKDUe9/wpe9ideC+cc1NmY3SNNi5TDsMt6RhA6q6BLpw81phg+Q
+# urb-block/1 mSQGPbOnIuy51jzArMlNd/CYZ26tLNZ0T0H41zhDwiyaqM3Z2mTBqJ12fVbQsLJ2JKFJa7bvJibCjWetm6JLcwWOfUn4LwP+
+# urb-block/1 bgGBoY5BEzu8fsg6hYBwNyh1/S/fm+L47nsTSRLHrSdPNmwixnBSM7dgk0aHDkpf35LfN0mPIpX2xZWi3vYUg3dDXZiIvDSJ
+# urb-block/1 otiuyxiGHtclLW3gVT7dwi+ue7Oo3tOafvzrLrue9x8=
